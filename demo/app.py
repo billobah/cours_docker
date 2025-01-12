282ef74f72413b3dde99ba19e4f83acd92d8f569
Billo Bah <billobah@yahoo.fr>
2025-01-12 10:16:53 +0100

    building my first docker image

---
+++ demo/app.py
@@ -0,0 +1,7 @@
+def main():
+    print('je suis le script')
+
+# Avec le code ci-dessus, à l'import de app.py, on pourra exécuter directement la fonction
+# sans devoir la charger puis l'appeler. A l'import de app.py, le code sera exécuté directement
+if __name__ == '__main__':
+    main()
