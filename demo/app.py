d4071516f62577aa69f33fa9d0f4b803e29ada37
Billo Bah <billobah@yahoo.fr>
2025-01-13 11:12:35 +0100

    changing app.py

--- demo/app.py
+++ demo/app.py
@@ -1,5 +1,5 @@
 def main():
-    print('je suis le script!')
+    print('je suis le script maj 1.0.1')
 
 # Avec le code ci-dessus, à l'import de app.py, on pourra exécuter directement la fonction
 # sans devoir la charger puis l'appeler. A l'import de app.py, le code sera exécuté directement
